7972ffcf8dec78dc055ea8839818d19ab8608df7
Balogun Muhammad Awwal <balogunmuhammadawwal@gmail.com>
2024-06-26 14:39:38 +0100

    chore: update top nav

--- components/UI/uiStyles.ts
+++ components/UI/uiStyles.ts
@@ -1,4 +1,4 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import { ClampComponent } from '../../styles/clampBuilder';
 
 export const StyledIConContainer = styled.span`
@@ -12,4 +12,21 @@ export const StyledIConContainer = styled.span`
   align-items: center;
 
   border-radius: 50%;
+`;
+
+export const FlexContainer = styled.div<{
+  $flow: string;
+  $size: 'tiny' | 'small' | 'medium' | 'large';
+}>`
+  display: flex;
+
+  gap: ${() => ClampComponent(900, 1250, 2, 2.5)};
+
+  flex-flow: ${prop => (prop.$flow === 'col' ? 'column' : 'row')};
+
+  gap: ${prop => prop.$size === 'large' && ClampComponent(900, 1250, 3, 3.5)};
+  gap: ${prop => prop.$size === 'small' && ClampComponent(900, 1250, 2, 2.5)};
+  gap: ${prop => prop.$size === 'tiny' && ClampComponent(900, 1250, 1.5, 2)};
+  gap: ${prop =>
+    prop.$size === 'medium' && ClampComponent(900, 1250, 0.8, 1.2)};
 `;
