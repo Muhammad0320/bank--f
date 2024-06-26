9e43e2e26502715e49ee690fe34b833f72299e99
Balogun Muhammad Awwal <balogunmuhammadawwal@gmail.com>
2024-06-26 14:21:25 +0100

    chore: update top nav

--- components/nav/TopNav.tsx
+++ components/nav/TopNav.tsx
@@ -1,3 +1,23 @@
-const TopNav = () => {};
+import IconContainer from '../UI/IconContainer';
+import { StyledH2, Text } from '../UI/Text';
+import { FlexContainer } from '../UI/uiStyles';
+import { StyledTopNav } from './NavStyles';
+
+const TopNav = () => {
+  return (
+    <StyledTopNav>
+      <FlexContainer>
+        <StyledH2> Hey, Muhammad 👋 </StyledH2>
+        <Text> Welcome back to YourBank </Text>
+      </FlexContainer>
+
+      <FlexContainer>
+        <IconContainer> N </IconContainer>
+        <IconContainer> T </IconContainer>
+        <IconContainer> M </IconContainer>
+      </FlexContainer>
+    </StyledTopNav>
+  );
+};
 
 export default TopNav;
