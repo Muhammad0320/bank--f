c5a05e245624987ac3e86eb4582c34077d0a737b
Balogun Muhammad Awwal <balogunmuhammadawwal@gmail.com>
2024-06-26 14:16:47 +0100

    chore:  add text ui styles

---
+++ components/UI/Text.ts
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+import { ClampComponent } from '../../styles/clampBuilder';
+
+export const StyledH1 = styled.h1`
+  color: var(--black-color);
+  font-weight: 600;
+  font-size: ${() => ClampComponent(900, 1250, 2.5, 3.5)};
+`;
+
+export const StyledH2 = styled.h2`
+  color: var(--black-color);
+
+  font-weight: 500;
+
+  font-size: ${() => ClampComponent(900, 1250, 1.5, 2.5)};
+`;
