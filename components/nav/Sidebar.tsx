4b782c80aee832195e87c34c52dab740d0ce37f3
Balogun Muhammad Awwal <balogunmuhammadawwal@gmail.com>
2024-06-27 15:52:29 +0100

    chore: updatesidebar

--- components/nav/Sidebar.tsx
+++ components/nav/Sidebar.tsx
@@ -3,21 +3,30 @@ import NavLink from './NavLink';
 import NavLinks from './NavLinks';
 import { StyledSidebar } from './NavStyles';
 
+import { GoSignOut } from 'react-icons/go';
+import { HiOutlineCog } from 'react-icons/hi';
+
 const Sidebar = () => {
   return (
     <StyledSidebar>
       <NavLinks>
-        {navData.map(item => (
-          <NavLink key={item.content} href={item.href}>
+        {navData.map(({ content, icon, href }) => (
+          <NavLink key={content} href={href} icon={icon}>
             {' '}
-            {item.content}{' '}
+            {content}{' '}
           </NavLink>
         ))}
       </NavLinks>
 
       <NavLinks>
-        <NavLink href="/settings"> Settings </NavLink>
-        <NavLink href="/logout"> Logout </NavLink>
+        <NavLink icon={HiOutlineCog} href="/settings">
+          {' '}
+          Settings{' '}
+        </NavLink>
+        <NavLink icon={GoSignOut} href="/logout">
+          {' '}
+          Logout{' '}
+        </NavLink>
       </NavLinks>
     </StyledSidebar>
   );
