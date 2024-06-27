8ed8c4a952ef476157c44bb802a97d54fbdb13de
Balogun Muhammad Awwal <balogunmuhammadawwal@gmail.com>
2024-06-27 15:49:02 +0100

    chore: update input ts

--- components/nav/NavData.ts
+++ components/nav/NavData.ts
@@ -1,26 +1,45 @@
-export const navData: { href: string; content: string }[] = [
+import { type ReactNode } from 'react';
+
+import { MdOutlineAccountBalance } from 'react-icons/md';
+import { TbReportAnalytics } from 'react-icons/tb';
+
+import { GoSignOut } from 'react-icons/go';
+import { LuLayoutDashboard } from 'react-icons/lu';
+import { IoMoonOutline } from 'react-icons/io5';
+import { IoSunnyOutline } from 'react-icons/io5';
+import { HiOutlineCog } from 'react-icons/hi';
+import { CiCreditCard2 } from 'react-icons/ci';
+import { GrTransaction } from 'react-icons/gr';
+import { IconType } from 'react-icons';
+
+export const navData: { href: string; content: string; icon: IconType }[] = [
   {
     href: '/dashboard',
     content: 'Dashboard',
+    icon: LuLayoutDashboard,
   },
 
   {
     href: '/transaction',
     content: 'Tracsaction',
+    icon: GrTransaction,
   },
 
   {
     href: '/report',
     content: 'Report',
+    icon: TbReportAnalytics,
   },
 
   {
     href: '/card',
     content: 'Card',
+    icon: CiCreditCard2,
   },
 
   {
     href: '/account',
     content: 'Account',
+    icon: MdOutlineAccountBalance,
   },
 ];
