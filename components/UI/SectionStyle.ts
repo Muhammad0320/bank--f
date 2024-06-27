ee0a0861d1a1ef6b44d8b43a07fb842f217a16f2
Balogun Muhammad Awwal <balogunmuhammadawwal@gmail.com>
2024-06-27 15:17:39 +0100

    chore: update page tsx

--- components/UI/SectionStyle.ts
+++ components/UI/SectionStyle.ts
@@ -1,3 +1,5 @@
+'use client';
+
 import styled from 'styled-components';
 
 export const SectionStyle = styled.section`
--- src/app/(content)/settings/page.tsx
+++ src/app/(content)/settings/page.tsx
@@ -1,5 +1,29 @@
+import Input from '../../../../components/UI/Input';
+import { SectionStyle } from '../../../../components/UI/SectionStyle';
+import { StyledH1, StyledH3 } from '../../../../components/UI/Text';
+import { FlexContainer } from '../../../../components/UI/uiStyles';
+
 const SettingsPage = () => {
-  return <section>This is the serting page</section>;
+  return (
+    <SectionStyle>
+      <FlexContainer $flow="column" $size="medium">
+        <StyledH1>Settings</StyledH1>
+
+        {/*  For the Profile settings  */}
+        <FlexContainer $flow="column" $size="small">
+          <StyledH3> Profile Settings </StyledH3>
+          <Input label="Email" id="name" placeholder="John Doe" type="text" />
+          <Input
+            label="Your email address"
+            id="email"
+            type="email"
+            placeholder="johndoe@gmail.com"
+          />
+          <Input label="Avatar" id="avatar" type="file" />
+        </FlexContainer>
+      </FlexContainer>
+    </SectionStyle>
+  );
 };
 
 export default SettingsPage;
