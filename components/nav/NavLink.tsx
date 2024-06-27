801efdcd61676e91829f3b7e98bdfc09283cfd4b
Balogun Muhammad Awwal <balogunmuhammadawwal@gmail.com>
2024-06-27 15:39:29 +0100

    chore: update navLink

--- components/nav/NavLink.tsx
+++ components/nav/NavLink.tsx
@@ -1,12 +1,20 @@
 import Link from 'next/link';
-import { FC, PropsWithChildren } from 'react';
+import { FC, PropsWithChildren, ReactNode } from 'react';
+import { NavLinkContainer } from './NavStyles';
 
 type NavLinkType = PropsWithChildren & {
   href: string;
+  icon: ReactNode;
 };
 
-const NavLink: FC<NavLinkType> = ({ children, href }) => {
-  return <Link href={href}> {children} </Link>;
+const NavLink: FC<NavLinkType> = ({ children, href, icon }) => {
+  return (
+    <NavLinkContainer>
+      {icon}
+
+      <Link href={href}> {children} </Link>
+    </NavLinkContainer>
+  );
 };
 
 export default NavLink;
