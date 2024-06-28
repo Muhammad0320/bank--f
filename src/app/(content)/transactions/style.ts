08e0a88d35c5f1f546cb29abc3d313813b03c695
Balogun Muhammad Awwal <balogunmuhammadawwal@gmail.com>
2024-06-28 17:25:14 +0100

    chore: update style ts

--- src/app/(content)/transactions/style.ts
+++ src/app/(content)/transactions/style.ts
@@ -7,12 +7,20 @@ import { ClampComponent } from '../../../../styles/clampBuilder';
 export const TransactionContainer = styled(MainContainer)`
   display: grid;
 
-  grid-template-columns: 1.2fr 1fr;
+  grid-template-columns: auto 1.2fr 1fr;
   grid-template-rows: 1.2fr 1fr;
 
   gap: ${() => ClampComponent(900, 1250, 2.5, 3.5)};
 
   & > * {
     box-shadow: var(--box-shadow-light);
+
+    border: 1px solid var(--card-color);
+
+    padding: ${() => ClampComponent(900, 1250, 1, 2)};
+
+    border-radius: ${() => ClampComponent(900, 1250, 1, 1.4)};
   }
 `;
+
+
