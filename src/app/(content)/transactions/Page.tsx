90d04591c9a102954e60a438e45203983f0d6942
Balogun Muhammad Awwal <balogunmuhammadawwal@gmail.com>
2024-06-28 18:05:13 +0100

    chore:  update page tsx

--- src/app/(content)/transactions/Page.tsx
+++ src/app/(content)/transactions/Page.tsx
@@ -43,6 +43,23 @@ const Tracsaction = () => {
             <Text $size="small"> 12/06/2024 </Text>
           </FlexContainer>
         </TxnCard>
+        <TxnCard $flow="row" $size="small">
+          <AvatarContainer> I </AvatarContainer>
+
+          <FlexContainer
+            $flow="column"
+            $size="tiny"
+            style={{ marginRight: 'auto' }}
+          >
+            <Text> Muhammad Awwal </Text>
+            <Text $size="small"> Shit money </Text>
+          </FlexContainer>
+
+          <FlexContainer $flow="column" $size="tiny">
+            <Text> - $13.00 </Text>
+            <Text $size="small"> 12/06/2024 </Text>
+          </FlexContainer>
+        </TxnCard>
       </TxnHistory>
     </TransactionContainer>
   );
