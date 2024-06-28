924a7ca3e5ae723ccec648a790125ea077bdb54c
Balogun Muhammad Awwal <balogunmuhammadawwal@gmail.com>
2024-06-28 12:52:06 +0100

    chore:  update page

--- src/app/(content)/settings/page.tsx
+++ src/app/(content)/settings/page.tsx
@@ -28,9 +28,35 @@ const SettingsPage = () => {
         <FlexContainer $flow="column" $size="small">
           <StyledH3> Account Settings </StyledH3>
 
+          <FlexContainer $flow="row" $size="small">
+            <Input label="Current pin" id="currentPin" type="password" />
+            <Input label="New pin" id="newPin" type="password" />
+            <Input label="Confirm pin" id="confirmPin" type="password" />
+          </FlexContainer>
+
           <Text> Disply Account Number </Text>
           <Text> Disply Account Balace </Text>
           <Text> Disply Account Type </Text>
+        </FlexContainer>
+
+        {/*  Security settings  */}
+
+        <FlexContainer $flow="column" $size="small">
+          <StyledH3>Security Settings</StyledH3>
+
+          <FlexContainer $flow="row" $size="small">
+            <Input
+              label="Current Password"
+              id="currentPassword"
+              type="password"
+            />
+            <Input label="New Password" id="newPassword" type="password" />
+            <Input
+              label="Confirm Password"
+              id="confirmPassword"
+              type="password"
+            />
+          </FlexContainer>
         </FlexContainer>
       </FlexContainer>
     </SectionStyle>
