50b9db1a86d756c0a1c33c09eb583f05eccd30e7
Balogun Muhammad Awwal <balogunmuhammadawwal@gmail.com>
2024-06-29 14:59:32 +0100

    chore:  update srtle ts

--- src/app/style.ts
+++ src/app/style.ts
@@ -19,5 +19,18 @@ export const FirstDashBoardRow = styled.section`
 
   grid-template-columns: 2fr repeat(2, 1fr);
 
+  grid-template-rows: 1fr 10rem;
+
   gap: ${() => ClampComponent(900, 1250, 2, 3)};
 `;
+
+export const LastDashboardRow = styled.section`
+  grid-row: 2 / -1;
+
+  display: grid;
+
+  grid-template-columns: 1fr 1.2fr 1fr;
+
+  grid-template-rows: 10rem 1fr;
+`;
+
