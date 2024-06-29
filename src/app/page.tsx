35f8cb4bce69a2e551323a74b3d2e5083ab63c89
Balogun Muhammad Awwal <balogunmuhammadawwal@gmail.com>
2024-06-29 15:29:25 +0100

    chore:  update page comp

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,58 @@
-const Page = () => {
-  return <h1>Hello world</h1>;
+import React from 'react';
+import {
+  BalaceSummary,
+  DashBoardContainer,
+  FirstDashBoardRow,
+  LastDashboardRow,
+  MonthlyCardContainer,
+} from './style';
+import { StyledH3, Text } from '../../components/UI/Text';
+import { FlexContainer } from '../../components/UI/uiStyles';
+import IconContainer from '../../components/UI/IconContainer';
+
+const Page: React.FC = () => {
+  return (
+    <DashBoardContainer>
+      <FirstDashBoardRow>
+        <BalaceSummary $flow="column" $size="small">
+          <StyledH3> Yearly Balance summary </StyledH3>
+          <div> The Chart </div>
+        </BalaceSummary>
+
+        <MonthlyCardContainer $flow="column" $size="medium">
+          <FlexContainer $flow="row" $size="btw">
+            <FlexContainer $flow="column" $size="tiny">
+              <StyledH3> $45,000.00 </StyledH3>
+              <Text> Income </Text>
+              <Text $size="small"> +0.5% than last month </Text>
+            </FlexContainer>
+            {/* Progress bar */}
+            <IconContainer> PB </IconContainer>
+          </FlexContainer>
+          <FlexContainer $flow="row" $size="btw">
+            <FlexContainer $flow="column" $size="tiny">
+              <StyledH3> $45,000.00 </StyledH3>
+              <Text> Income </Text>
+              <Text $size="small"> +0.5% than last month </Text>
+            </FlexContainer>
+            {/* Progress bar */}
+            <IconContainer> PB </IconContainer>
+          </FlexContainer>
+          <FlexContainer $flow="row" $size="btw">
+            <FlexContainer $flow="column" $size="tiny">
+              <StyledH3> $45,000.00 </StyledH3>
+              <Text> Income </Text>
+              <Text $size="small"> +0.5% than last month </Text>
+            </FlexContainer>
+            {/* Progress bar */}
+            <IconContainer> PB </IconContainer>
+          </FlexContainer>
+        </MonthlyCardContainer>
+      </FirstDashBoardRow>
+
+      <LastDashboardRow></LastDashboardRow>
+    </DashBoardContainer>
+  );
 };
 
 export default Page;
