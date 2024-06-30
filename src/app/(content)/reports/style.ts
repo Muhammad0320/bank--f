fc6ff6172503997f747265e027c92036b49b72f9
Balogun Muhammad Awwal <balogunmuhammadawwal@gmail.com>
2024-06-30 04:15:31 +0100

    chore:  update style ts

--- src/app/(content)/reports/style.ts
+++ src/app/(content)/reports/style.ts
@@ -8,6 +8,7 @@ import { FlexContainer } from '../../../../components/UI/uiStyles';
 export const ReportContainer = styled(MainContainer)`
   display: grid;
 
+
   grid-template-columns: repeat(4, 1fr) 1.2fr;
 
   grid-template-rows: 1fr 3fr 1fr;
@@ -17,11 +18,16 @@ export const ReportContainer = styled(MainContainer)`
 
 export const FirstReportRow = styled(FlexContainer)`
   grid-row: 1/ 2;
-  grid-column: 1 / 5;
+  grid-column: 1 / -1;
 
   gap: inherit;
 
   & > * {
+    padding: ${() => ClampComponent(900, 1250, 1, 1.5)};
+
+    border-radius: ${() => ClampComponent(900, 1250, 1, 1.4)};
+
+    box-shadow: var(--box-shadow-light);
   }
 `;
 
