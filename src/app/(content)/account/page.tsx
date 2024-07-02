ee542fda04f2e9b0d38efc7d96074ebc11500c2b
Balogun Muhammad Awwal <balogunmuhammadawwal@gmail.com>
2024-07-02 17:04:04 +0100

    chore:  update page tsx

--- src/app/(content)/account/page.tsx
+++ src/app/(content)/account/page.tsx
@@ -1,5 +1,31 @@
+import IconContainer from '../../../../components/UI/IconContainer';
+import { Text } from '../../../../components/UI/Text';
+import { FlexContainer } from '../../../../components/UI/uiStyles';
+import {
+  AccountContainer,
+  AccountDetailsComponent,
+  AccountOverviewContainer,
+} from './style';
+
 const AccountPage = () => {
-  return <div> Acount page </div>;
+  return (
+    <AccountContainer>
+      <AccountOverviewContainer></AccountOverviewContainer>
+
+      <AccountDetailsComponent as="section" $flow="column" $size="small">
+        <FlexContainer $flow="column" $size="tiny">
+          <Text> Income </Text>
+          <FlexContainer $flow="row" $size="tiny">
+            <IconContainer> I </IconContainer>
+
+            <Text> $12,000 </Text>
+
+            <div>I m gonna be the graph</div>
+          </FlexContainer>
+        </FlexContainer>
+      </AccountDetailsComponent>
+    </AccountContainer>
+  );
 };
 
 export default AccountPage;
