a7cd919a3ee3790c1f631ea59c79a94c416d9c79
Balogun Muhammad Awwal <balogunmuhammadawwal@gmail.com>
2024-07-02 17:10:00 +0100

    chore:  update page tsx

--- src/app/(content)/account/page.tsx
+++ src/app/(content)/account/page.tsx
@@ -23,6 +23,53 @@ const AccountPage = () => {
             <div>I m gonna be the graph</div>
           </FlexContainer>
         </FlexContainer>
+
+        <FlexContainer $flow="column" $size="tiny">
+          <Text> Income </Text>
+          <FlexContainer $flow="row" $size="tiny">
+            <IconContainer> I </IconContainer>
+
+            <Text> $12,000 </Text>
+
+            <div>I m gonna be the graph</div>
+          </FlexContainer>
+        </FlexContainer>
+
+        <FlexContainer $flow="column" $size="small" as="ul">
+          <FlexContainer $flow="row" $size="btw" as={'li'}>
+            <Text> Account Status </Text>
+            <Text $size="small"> Active </Text>
+          </FlexContainer>
+          <FlexContainer $flow="row" $size="btw" as={'li'}>
+            <Text> Account No </Text>
+            <Text $size="small"> 9640253013 </Text>
+          </FlexContainer>
+          <FlexContainer $flow="row" $size="btw" as={'li'}>
+            <Text> Pin </Text>
+            <Text $size="small"> .... </Text>
+          </FlexContainer>
+          <FlexContainer $flow="row" $size="btw" as={'li'}>
+            <Text> Account Name </Text>
+            <Text $size="small"> Balogun Muhammad </Text>
+          </FlexContainer>
+          <FlexContainer $flow="row" $size="btw" as={'li'}>
+            <Text> BVN </Text>
+            <Text $size="small"> 2291202020 </Text>
+          </FlexContainer>
+          <FlexContainer $flow="row" $size="btw" as={'li'}>
+            <Text> DOB </Text>
+            <Text $size="small"> 20rd Nov. 2005 </Text>
+          </FlexContainer>
+          <FlexContainer $flow="row" $size="btw" as={'li'}>
+            <Text> Phone </Text>
+            <Text $size="small"> 09166537641 </Text>
+          </FlexContainer>
+
+          <FlexContainer $flow="row" $size="btw" as={'li'}>
+            <Text> Address </Text>
+            <Text $size="small"> G50, Balogun compd... </Text>
+          </FlexContainer>
+        </FlexContainer>
       </AccountDetailsComponent>
     </AccountContainer>
   );
