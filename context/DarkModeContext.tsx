07abb708b0bcd9368ba1f532eb29a2c9324c56b4
Balogun Muhammad Awwal <balogunmuhammadawwal@gmail.com>
2024-07-04 16:48:21 +0100

    chore:  update dark mode logic

--- context/DarkModeContext.tsx
+++ context/DarkModeContext.tsx
@@ -1,4 +1,4 @@
-import { createContext, type ReactNode } from 'react';
+import { createContext, useEffect, type ReactNode } from 'react';
 import { useLocalStorageState } from '../hooks/useLocalStorageState';
 
 interface DarkModeType {
@@ -10,6 +10,16 @@ const DarkModeContext = createContext<DarkModeType | null>(null);
 
 export const DarkModeProvider = ({ children }: { children: ReactNode }) => {
   const [isDarkMode, setIsDarkMode] = useLocalStorageState(false, 'isDarkMode');
+
+  useEffect(() => {
+    if (isDarkMode) {
+      document.documentElement.classList.add('dark-mode');
+      document.documentElement.classList.remove('light-mode');
+    } else {
+      document.documentElement.classList.add('light-mode');
+      document.documentElement.classList.remove('dark-mode');
+    }
+  });
 
   if (typeof setIsDarkMode !== 'function') return;
 
