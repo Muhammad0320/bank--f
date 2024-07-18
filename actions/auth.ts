9a1e3451b5b3c043282f12b2980f5785ab781f4e
Balogun Muhammad Awwal <balogunmuhammadawwal@gmail.com>
2024-07-18 04:54:47 +0100

    chore:  update auth ts

--- actions/auth.ts
+++ actions/auth.ts
@@ -1,1 +1,10 @@
+import z from 'zod';
+
+const UserSchema = z.object({
+  name: z.string({ message: 'Invalid name' }),
+  email: z.string().email('Invalid email format'),
+});
+
+type User = z.infer<typeof UserSchema>;
+
 export const signupApi = () => {};
