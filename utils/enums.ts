dbf1efcefa5ba4752dc4762746eaa4d6134fa85a
Balogun Muhammad Awwal <balogunmuhammadawwal@gmail.com>
2024-07-18 05:13:53 +0100

    chore: update code

--- utils/enums.ts
+++ utils/enums.ts
@@ -1,13 +1,3 @@
-export type UserUpdatesObj = {
-  timeStamp: Date;
-
-  old: string;
-
-  new: string;
-
-  updatedField: string;
-};
-
 export enum UserStatus {
   Idle = 'idle',
   Active = 'active',
