20992c513f5743b91465089c47f5af1c62975969
Balogun Muhammad Awwal <balogunmuhammadawwal@gmail.com>
2024-07-18 05:00:57 +0100

    chore: update auth ts

--- actions/auth.ts
+++ actions/auth.ts
@@ -1,9 +1,15 @@
 import z from 'zod';
 
 const UserSchema = z.object({
-  name: z.string({ message: 'Invalid name' }),
+  name: z.string({ message: 'Invalid name format' }),
   email: z.string().email('Invalid email format'),
+  phone: z.number().min(13, 'Invalid phone format'),
+  password: z.string().min(8, 'Password too short'),
+  passwordConfirm: z.string(),
+  avatar: z.any(),
 });
+
+
 
 type User = z.infer<typeof UserSchema>;
 
