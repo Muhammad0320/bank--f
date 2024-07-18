83c9d814ffecd1c6ae72558e2ba1451dc5c50355
Balogun Muhammad Awwal <balogunmuhammadawwal@gmail.com>
2024-07-18 16:05:38 +0100

    chore:  update button ts

--- components/UI/Button.tsx
+++ components/UI/Button.tsx
@@ -1,9 +1,15 @@
 import { ComponentPropsWithRef, ReactNode } from 'react';
+import { useFormStatus } from 'react-dom';
 
-type ButtonType = ComponentPropsWithRef<'button'> & { children: ReactNode };
+type ButtonType = ComponentPropsWithRef<'button'> & {
+  children: ReactNode;
+  pending: string;
+};
 
-const Button = ({ children, ...props }: ButtonType) => {
-  return <button {...props}> {children} </button>;
+const Button = ({ children, pending: pendingText, ...props }: ButtonType) => {
+  const { pending } = useFormStatus();
+
+  return <button {...props}> {pending ? pendingText : children} </button>;
 };
 
 export default Button;
