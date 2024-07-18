44876f384f95396c4c17d2639c04aea8eac5424f
Balogun Muhammad Awwal <balogunmuhammadawwal@gmail.com>
2024-07-18 17:09:34 +0100

    chore: update page tsx

--- src/app/(content)/auth/signup/page.tsx
+++ src/app/(content)/auth/signup/page.tsx
@@ -1,13 +1,22 @@
+import { useFormState } from 'react-dom';
 import Input from '../../../../../components/UI/Input';
+import { signupAction } from '../../../../../actions/auth';
+import { FormErrorObj } from '../../../../../utils/types';
 
 const FormPage = () => {
+  const initialState: any = { message: [] };
+
+  const [formAction, state] = useFormState(signupAction, initialState);
+
   return (
     <div>
-      <Input id="name" label="name" type="string" />
-      <Input id="email" label="email" type="email" />
-      <Input id="password" label=" password" type="password" />
-      <Input id="passwordConfirm" label="password confirm" type="password" />
-      <Input id="phone" label="phone" type="number" />
+      <form action={formAction}>
+        <Input id="name" label="name" type="string" />
+        <Input id="email" label="email" type="email" />
+        <Input id="password" label=" password" type="password" />
+        <Input id="passwordConfirm" label="password confirm" type="password" />
+        <Input id="phone" label="phone" type="number" />
+      </form>
     </div>
   );
 };
